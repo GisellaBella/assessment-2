0378f98a801f176e299c203a4050da7a2bb38e2c
Gisellabella <gisella@gmail.com>
2016-11-18 12:07:14 -0700

    coming along

--- bikerace/app.js
+++ bikerace/app.js
@@ -1,34 +1,78 @@
 window.onload=function(){
-
+	var margin = 0;
 	var slowCyclist = document.getElementById("slowCyclist");
-	//var fastCyclist;
+	var fastCyclist = document.getElementById("fastCyclist");
 
 	document.addEventListener('keydown', moveCyclist);
 
 	function moveCyclist(e) {
+
 		e = e || window.event;
 		if (e.keyCode == '37') { //left arrow
-	    	moveLeft();
+	    slowGuy();	    
+	    console.log("left arrow");
 	    }
-	    else if (e.keyCode == '39') { //right arrow
-	        moveRight();
-		}	
-	}	
-	moveCyclist();
 
-	function moveLeft() {
-		var margin = 20;
-		margin --;//moving left
-		document.getElementById("slowCyclist").style.marginLeft = margin - "px";
+	    else if (e.keyCode == '38') { //up arrow
+	    slowGuyFaster();
+	    fastGuyWind();
+	    console.log("up arrow");
+	    }	
+
+	   	 else if (e.keyCode == '39') { //right arrow
+	    fastGuy();
+	    slowGuyWind();   	    
+	    console.log("right arrow");
+		}
+		
+		 else if (e.keyCode == '40') { //down arrow
+	        fastGuyFaster();    	    
+	    console.log("down arrow");
+		}		
+	}	
+
+	function slowGuy() {
+		margin = margin+5;
+		margin ++;//moving right
+		document.getElementById("slowCyclist").style.marginLeft = margin + "px";
 		}
 
 	           
-	 function moveRight() {
-	 	var margin = 20;
-		margin ++;//moving left
+	 function slowGuyFaster() {
+	 	margin = margin+15;
+		margin ++;//moving right
 	    document.getElementById("slowCyclist").style.marginLeft = margin + "px";
-	    console.log("So, you've clicked the right arrow");
 		}
+
+	function fastGuy() {
+		margin = margin+2;
+		margin ++;//moving right
+		document.getElementById("fastCyclist").style.marginLeft = margin + "px";
+		}
+
+	function fastGuyFaster() {
+		margin = margin+12;
+		margin ++;//moving right
+		document.getElementById("fastCyclist").style.marginLeft = margin + "px";
+		}
+
+	function fastGuyWind() {
+		margin = margin-5;
+		margin ++;//loses speed
+		document.getElementById("fastCyclist").style.marginLeft = margin + "px";
+		}
+
+	function slowGuyWind() {
+		margin = margin-2;
+		margin ++;//loses speed
+		document.getElementById("slowCyclist").style.marginLeft = margin + "px";
+		}
+
+
+
+
+	
+	        
 	        
 
 
